363060a3abbee7e61e1e82109098064a09c7c1fa
KolluruSrinivas <srinivasraokolluru@gmail.com>
2024-07-16 20:56:13 -0400

    change

--- variables.tf
+++ variables.tf
@@ -1,34 +1,7 @@
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  default = "8258e319-97da-4600-a76c-49edbf93df29"
+variable "client_id" {
+  description = "Client ID for Azure service principal"
 }
 
-variable "location" {
-  description = "Azure region"
-  default     = "East US"  # Change to your desired region
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  default     = "RG-Terraform"
-}
-
-variable "virtual_network_name" {
-  description = "Name of the virtual network"
-  default     = "TerraformVirtualNetwork"
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-  default     = "myAKSSubnet"
-}
-
-variable "aks_cluster_name" {
-  description = "Name of the AKS cluster"
-  default     = "myAKSCluster"
-}
-
-variable "aks_kubernetes_version" {
-  description = "Kubernetes version for AKS cluster"
-  default     = "1.28.9"  # Change to your desired Kubernetes version
+variable "client_secret" {
+  description = "Client Secret for Azure service principal"
 }
